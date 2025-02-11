f6e4e5049438b272a79fc103b0266c8a8c720d50
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-02-11 14:10:10 +0200

    add Test1

---
+++ Test1Clone/Test1Clone/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] rand = new int[10];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < rand.Length; i++)
+{
+    rand[i] = rnd.Next(-10, 10);
+    Console.WriteLine($"Massive so vsemi indeksami {i}: {rand[i]}");
+}
+Console.WriteLine("");
+for (int i = 0; i < rand.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"Parniy indeks {i}: {rand[i]}");
+        sum += rand[i];
+    }
+}
+if (sum > 0)
+{
+    Console.WriteLine($"summa: {sum}");
+}
+else
+{
+    Console.WriteLine($"Summa otricatelna: {sum}");
+}
